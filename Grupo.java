fca7a1b81e022bfae1c4ed99069bd47d10d78550
muriosh <75389767+muriosh@users.noreply.github.com>
2024-02-02 00:19:32 +0100

    Modelo Grupo
    Clase Grupo para App de validación del Modelo

---
+++ Grupo.java
@@ -0,0 +1,51 @@
+package crud;
+
+
+
+public class Grupo {
+	private String ies;
+	private String ciclo;
+	private String curso;
+	
+	public Grupo(String ies, String ciclo, String curso) {
+		this.ies= ies;
+		this.ciclo =ciclo;
+		this.curso= curso;
+	}
+	
+	
+	public String getIes() {
+		return ies;
+	}
+
+
+	public void setIes(String ies) {
+		this.ies = ies;
+	}
+
+
+	public String getCiclo() {
+		return ciclo;
+	}
+
+
+	public void setCiclo(String ciclo) {
+		this.ciclo = ciclo;
+	}
+
+
+	public String getCurso() {
+		return curso;
+	}
+
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Grupo [ies=" + ies + ", ciclo=" + ciclo + ", curso=" + curso + "]\n";
+	}
+}
